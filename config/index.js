2a883b78480232c52c95d8a12066c0a3ce9b3b40
Jacob García <rock2494@hotmail.com>
2018-08-29 13:08:34 -0500

    Added sectret and databaseURI

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
 module.exports = {
   project: {
-    name: 'React Boilerplate',
-    shortName: 'React Boilerplate',
-    themeColor: '#ffffff',
-    backgroundColor: '#ffffff',
+    name: "Visual Search ",
+    shortName: "VS",
+    themeColor: "#1d2229",
+    backgroundColor: "#1d2229"
   },
-}
+  secret: "SvP3-Rs.EcR3T{p455}",
+  databaseUri:
+    process.env.MONGODB_URL || "mongodb://localhost:27017/visualsearch"
+};

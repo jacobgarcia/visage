1c9a6e476da71d558c460eca60a8b5433e3bb493
Jacob García <rock2494@hotmail.com>
2018-08-31 17:45:55 -0500

    Router for images configured

--- router/v1/images.js
+++ router/v1/images.js
@@ -3,28 +3,8 @@ const express = require('express')
 const path = require('path')
 const winston = require('winston')
 const router = new express.Router()
-const multer = require('multer')
 const crypto = require('crypto')
 const mime = require('mime')
-const base64Img = require('base64-img')
-const shortid = require('shortid')
-
-const Debug = require(path.resolve('models/Debug'))
-const Exception = require(path.resolve('models/Exception'))
-const Company = require(path.resolve('models/Company'))
-
-// Storage object specs
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => callback(null, 'static/uploads'),
-  filename: (req, file, callback) => {
-    crypto.pseudoRandomBytes(16, (error, raw) => {
-      callback(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype))
-    })
-  }
-})
-
-// Upload object specs
-const upload = multer({ storage: storage }).array('photos', 3) // single file upload using this variable
 
 router.route('/images').get((req, res) => {
   return res.status(200).json({ success: true })
--- router/v1/index.js
+++ router/v1/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const path = require('path')
 const router = new express.Router()
 
+router.use(require(path.resolve('router/v1/images')))
+
 router.use((req, res) => {
   return res.status(400).json({ message: 'No route' })
 })

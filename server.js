35ed2efa1844e7a101fa31b4b2a053232836d88b
Jacob García <rock2494@hotmail.com>
2018-08-31 17:27:17 -0500

    Server.js refactor

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 /* eslint-env node */
-const express = require('express')
-const path = require('path')
+const express = require('express') // API framework
+const path = require('path') // Native node.js path resolver
 const helmet = require('helmet') // Basic headers protection
-const winston = require('winston')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const hpp = require('hpp')
+const hpp = require('hpp') // Protection against parameter pollution
+
+const winston = require('winston') // Logger tool
+const dotenv = require('dotenv') // Manager for evironment variables
+const mongoose = require('mongoose') // Mongo object modeling
+const bodyParser = require('body-parser') // Parser for incoming requests
 
 const {
   databaseUri,
   project: { name }
-} = require(path.resolve('config'))
-const v1 = require(path.resolve('router/v1'))
+} = require(path.resolve('config')) // Configuration variables
 
-const mode = process.env.NODE_ENV
-const isProduction = mode === 'production'
-
+/* Winston logger object */
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -26,11 +24,19 @@ const logger = winston.createLogger({
   ]
 })
 
+const v1 = require(path.resolve('router/v1')) // API router location
+
+/* Environment variables */
+const { PORT = 8080, SERVER_ONLY = false } = process.env
+const mode = process.env.NODE_ENV
+const isProduction = mode === 'production'
+
+/* Environment configuration based on file */
 dotenv.config({
   path: path.resolve(`config/.env${isProduction ? '.production' : '.development'}`)
 })
 
-const { PORT = 8080, SERVER_ONLY = false } = process.env
+/* Connecting to Database */
 
 mongoose
   .connect(
@@ -46,16 +52,17 @@ mongoose
     logger.error('\n|\n|  Could not connect to DB\n|')
   })
 
-const app = express()
+const app = express() // App definition
 
+/* App configurations */
 app.use(helmet())
 app.use(hpp())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.use('/v1', v1)
 
-if (!isProduction && !SERVER_ONLY) app.use(require(path.resolve('config/webpackDevServer')))
+if (!isProduction && !SERVER_ONLY) app.use(require(path.resolve('config/webpackDevServer'))) // Use webpackDevServer if development environment
+// Send compilated version for the index if has been build with server flag (yarn build:prod)
 if (isProduction && !SERVER_ONLY) {
   app.use(express.static(path.resolve('dist')))
   app.get('*', (req, res) => res.sendFile(path.resolve('dist/index.html')))

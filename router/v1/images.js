2fe978ebe4174c6faeb98cd5e58fdd530680a3de
Jacob García <rock2494@hotmail.com>
2018-09-19 13:07:05 -0500

    Added method to get all users

--- router/v1/images.js
+++ router/v1/images.js
@@ -293,6 +293,7 @@ router.route('/stats/petitions').get((req, res) => {
     })
 })
 
+// TODO: Add hasAccess flag
 router.route('/stats/users/billing').get((req, res) => {
   // Find all users
   User.find({})
@@ -339,6 +340,19 @@ router.route('/stats/users/billing').get((req, res) => {
       users.sort(($0, $1) => {
         $0.billing - $1.billing
       })
+      return res.status(200).json({ users })
+    })
+})
+
+router.route('/users').get((req, res) => {
+  User.find({})
+    .select('username fullname company email indexStatus')
+    .exec((error, users) => {
+      if (error) {
+        console.log('Could not fetch users', error)
+        return res.status(500).json({ error: { message: 'Could not fetch users' } })
+      }
+
       return res.status(200).json({ users })
     })
 })

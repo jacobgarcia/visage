515d075cb31bc170fdc04c395911461f28c9d269
César Guadarrama <cesarguadarrama@icloud.com>
2018-11-25 18:45:20 -0600

    Added nav data

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,44 +2,53 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-import UsageBar from '../../components/UsageBar'
+import UsageBar from 'components/UsageBar'
+
+import { UserContext } from 'utils/context'
 
 function Header(props) {
   const isInDashboard = props.location.pathname === '/'
+
   return (
-    <header>
-      <Link to={isInDashboard ? '/profile' : '/'}>
-        <div className="user-container">
-          <div>
-            <div className="profile-icon" />
-          </div>
-          {isInDashboard ? (
-            <div className="user-container--info">
-              <h3>Nombre Apellido</h3>
-              <span>Activo</span>
+    <UserContext.Consumer>
+      {(data) => (
+        <header>
+          <Link to={isInDashboard ? '/profile' : '/'}>
+            <div className="user-container">
+              <div>
+                <div className="profile-icon" />
+              </div>
+              {isInDashboard ? (
+                <div className="user-container--info">
+                  <h3>{data?.user?.name}</h3>
+                  <span>
+                    {data?.user?.apiKey?.active ? 'Activo' : 'Inactivo'}
+                  </span>
+                </div>
+              ) : (
+                <div>
+                  <span>Regresar a</span>
+                  <h3>Dashboard</h3>
+                </div>
+              )}
             </div>
-          ) : (
-            <div>
-              <span>Regresar a</span>
-              <h3>Dashboard</h3>
-            </div>
+          </Link>
+          {isInDashboard && (
+            <Fragment>
+              <div className="usage-limit">
+                <span>Límite de uso</span>
+                <UsageBar percentage={32} />
+              </div>
+              <div>
+                <span>Llave de API</span>
+                <input type="text" value={data?.user?.apiKey?.value} readOnly />
+              </div>
+            </Fragment>
           )}
-        </div>
-      </Link>
-      {isInDashboard && (
-        <Fragment>
-          <div className="usage-limit">
-            <span>Límite de uso</span>
-            <UsageBar percentage={32} />
-          </div>
-          <div>
-            <span>Llave de API</span>
-            <p>pbCI6Im1hcmlvQG51cmUubXgiLCJ1c2VybmFt</p>
-          </div>{' '}
-        </Fragment>
+          <div />
+        </header>
       )}
-      <div />
-    </header>
+    </UserContext.Consumer>
   )
 }
 
